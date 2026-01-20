ab800a9a12a2d22e004640c1353e634b1777b0cc
Seonguk <seonguk0104@outlook.kr>
2026-01-20 22:29:55 +0900

    Register Scope calculation

--- AssemblyStudy/HelloAssembler.asm
+++ AssemblyStudy/HelloAssembler.asm
@@ -1,18 +1,32 @@
 %include "io64.inc"
 
+
 section .text
 global main
 main:
     mov rbp, rsp; for correct debugging
     
-    mov eax, 0x12345
-    mov rbx, 0x1234567
-    ;mov cl, 0x00
-   ; mov cl, 0x00; 원래 rcx에는(rbx도) 초기값이 0x1로 되어있다. cl 자리인 최하위 1bye에서 1을 나타낸다. 00000001. 이게 왜 있는걸까? 거기서 오른쪽 최하위 바이트인 cl을 0x00으로 0으로 바꾸면 0x00이 된다.
-    mov ch, 0xff ; 8 bit 1 byte CH인데 왜 0xff를 하면 0xff01이 찍힐까? 초기값 cl 최하위 1 byte에 1이 있었기 때문에 남아있었다. 지금은 cl을 0으로 변경한 뒤 실행하면 0xFF00이 된다. 1) 0xFF를 넣었는데 왜 0xFF00이 레지스터에 찍힐까?
-    mov rdx, rbx
-    mov ah, 0x00;
-    mov al, 0x00 ; eax 32bit 4byte인 eax 0x12345에 al (오른쪽 최하위 1byte에 0000을 넣는다.) 그러면 0x12300. 그런데 왜 0x12345 16진수는 저 표기법으로 어떻게 나타내기에 오른쪽 최하위 1byte에 0을 넣으면 0x12300이 될까? 2) 0x5자리에서 연산을 하는법이 궁금하다.
+    ; 16진법 : 0~9를 나타낸 뒤 다음 숫자를 하나의 문자로 나타내기 위해 알파벳을 사용. 
+    mov rax, 0xff 
+    ; 0x2자리니까 1byte
+    ; 가장 하위부터 값을 채우니 rax(4줄 8byte)에서 ax (한 줄 2byte) 범위를 잡고 값을 채우겠다는 뜻. 그래도 0xff는 두 문자 합쳐서 8byte이기에 다 쓰지 않고 al 자리에 ff.
+    mov rax, 0x0f 
+    ; 0x2자리니까 1byte
+    ;  똑같은 rax 범위를 잡고 al 자리에 0과 f를 넣으면 덮어씌워져 0x0f의 값이 되는 rax
+    mov ah, 0xee
+    ; 0x2자리니까 1byte. al 자리에 
+    ; 이번엔 al 자리에 0x0f가 있고 ah 자리에 0xee가 있으니 0xee0f가 된다. 
+    mov ax, 0xddee
+    ; 그리고 다시 ah (1)와 al (1)을 합친 ax(2)에 0xdd를 넣어도 0xeedd.
+    ; 만약 ax 범위를 잡고 꽉 채워 넣으면 0x4자리수를 넣어야 하는데 0xddee를 넣으면 0xddee가 된다.
+    ; 꿀팁) 0x2자리는 1byte = 1자리 당 4byte. 0x4자리는 2byte, 0x5자리는 = 20bit..
+    mov ah, 0x11
+    ; 0xddee에서 ah에 0x11을 하면 왼쪽 1byte이므로 0x11ee
+    ; 0xeedd에서 0x11을 하면 0x11dd
+    mov al, 0x22   
+    ; 0x11ee에서 al 오른쪽 1byte 22를 넣으면 0x1122
+    ; 0x11dd에서 al 오른쪾 1byte 22를 넣으면 0x1122
+   
     xor rax, rax
     ret
     
